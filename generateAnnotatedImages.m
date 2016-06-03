4e46cb3f2776c306707bf3d9e58fb2938f2b425d
Thanasis <tsiarasa@hotmail.com>
2016-06-03 11:47:34 +0100

    Create generateAnnotatedImages.m

---
+++ generateAnnotatedImages.m
@@ -0,0 +1,48 @@
+
+%get all images
+images=dir('DataSet/*.png');
+%get all xmls files
+xmls=dir('DataSet/*.xml');
+
+for i=1:length(images)
+    
+    %read a new image
+    I=imread(cat(2, 'DataSet/', images(i).name));
+    I = rgb2gray(I);
+    [rows,cols]=size(I);
+    
+    %read the respective xml file
+    xmlFile=cat(2, 'DataSet/', xmls(i).name);
+    
+    % forbidden lines
+    r = 0.8;
+    forbidWid = round(r*cols);
+    forbidHei = round(r*rows);
+    forbiddenLines.forbidWidS = floor((cols-forbidWid)/2)+1;
+    forbiddenLines.forbidWidE = forbiddenLines.forbidWidS+forbidWid-1;
+    forbiddenLines.forbidHeiS = floor((rows-forbidHei)/2)+1;
+    forbiddenLines.forbidHeiE = forbiddenLines.forbidHeiS+forbidHei-1;
+
+    [N, grTru, allPolygon, radius, polyCen, polyArea] = myXMLReader(rows, cols, I, xmlFile, forbiddenLines);
+    
+    %% plot the ground truth
+    figure;
+    imshow(I);
+    hold on;
+    axis equal;
+    axis([0 1024 0 1024]);
+
+    plot([1,forbiddenLines.forbidWidE,forbiddenLines.forbidWidE,cols], [forbiddenLines.forbidHeiS,forbiddenLines.forbidHeiS,forbiddenLines.forbidHeiE,forbiddenLines.forbidHeiE], 'r', 'linewidth',2);
+    hold on;
+    plot([forbiddenLines.forbidWidS,forbiddenLines.forbidWidS,forbiddenLines.forbidWidE], [forbiddenLines.forbidHeiS,forbiddenLines.forbidHeiE,forbiddenLines.forbidHeiE], 'r','linewidth',2);
+    hold on;
+
+    for i=1:N
+        xp = [allPolygon{i,1}(:,1); allPolygon{i,1}(1,1)];
+        yp = [allPolygon{i,1}(:,2); allPolygon{i,1}(1,2)];
+        plot(xp, yp, 'b', 'linewidth',1);
+        hold on;
+    end
+
+    hold off;
+end
