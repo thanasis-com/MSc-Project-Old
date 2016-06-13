88227d736ab6599141566ca3621d7fc7b3a0b19b
thanasis-com <tsiarasa@hotmail.com>
2016-06-13 19:34:34 +0100

    some new stuff

--- python/myTools.py
+++ python/myTools.py
@@ -1,4 +1,5 @@
 import theano
+import theano.tensor as T
 import numpy
 from PIL import Image
 
@@ -6,7 +7,6 @@ from os import listdir
 from os.path import isfile, join
 
 import numpy as np
-import theano
 import lasagne
 from lasagne import layers
 from lasagne.updates import nesterov_momentum
@@ -14,6 +14,9 @@ from nolearn.lasagne import NeuralNet
 from nolearn.lasagne import visualize
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
+
+import lasagne
+from lasagne.layers import InputLayer, Conv2DLayer, DenseLayer, MaxPool2DLayer, InverseLayer
 
 
 #imports all image names from a specific path and turns them into numpy arrays,
@@ -62,43 +65,86 @@ def oneDimension(images):
 	
 	return images
 
+#def crop(images, dimX, dimY):
+	
+
  
 
 def myConvNet(X_train, y_train):
-	
+	import numpy
 	myNet = NeuralNet(
-	    layers=[('input', layers.InputLayer),
-		    ('conv1', layers.Conv2DLayer),
-		    ('maxpool1', layers.MaxPool2DLayer),
-		    ('conv2', layers.Conv2DLayer),
-		    ('output', layers.MaxPool2DLayer),
-		    ],
-	    # input layer
-	    input_shape=(None, 1, 1024, 1024),
-	    # layer conv1
-	    conv1_num_filters=10,
-	    conv1_filter_size=(5, 5),
-	    conv1_nonlinearity=lasagne.nonlinearities.rectify,
-	    conv1_W=lasagne.init.GlorotUniform(),  
-	    # layer maxpool1
-	    maxpool1_pool_size=(2, 2),    
-	    # layer output
-	    conv2d2_num_filters=10,
-	    conv2d2_filter_size=(5, 5),
-	    conv2d2_nonlinearity=lasagne.nonlinearities.rectify,
+	        layers=[('input', layers.InputLayer),
+		        ('conv1', layers.Conv2DLayer),
+		        ('maxpool1', layers.MaxPool2DLayer),
+		        ('unpool1', layers.InverseLayer),
+		        ('output', layers.InverseLayer),
+		       ],
+	    	# input layer
+	    	input_shape=(None, 1, 819, 819),
+	    	# layer conv1
+	    	conv1_num_filters=10,
+	    	conv1_filter_size=(5, 5),
+	    	conv1_nonlinearity=lasagne.nonlinearities.rectify,
+	    	conv1_W=lasagne.init.GlorotUniform(),  
+	    	# layer maxpool1
+	    	maxpool1_pool_size=(2, 2),  
+	    
+	    	#layer unpool1
+	    	unpool1_layer=['maxpool1'],
+  
+	    	# layer output
+	    	output_layer=['conv1'],
 	   
-	    # optimization method params
-	    update=nesterov_momentum,
-	    update_learning_rate=theano.shared(float32(0.01)),
-	    update_momentum=theano.shared(float32(0.9)),
-	    max_epochs=5,
-	    verbose=1,
-	    )
+	    	# optimization method params
+	    	update=nesterov_momentum,
+	    	update_learning_rate=theano.shared(0.01),
+	    	update_momentum=theano.shared(0.9),
+	    	max_epochs=1,
+	    	verbose=1,
+	    	)
 	# Train the network
 	nn = myNet.fit(X_train, y_train)
 
+def myConvNet2(X_train, y_train):
+
+	l_in = InputLayer((None, 1, 819, 819), name="input_layer")
+	l1 = Conv2DLayer(l_in, num_filters=10, filter_size=5, name="convolutional1")
+	l2 = MaxPool2DLayer(l1, pool_size=2, name="maxpooling1")
+	l_u2 = InverseLayer(l2, l2, name="unpooling1")
+	l_u1 = InverseLayer(l_u2, l1, name="deconvolution1")
+
+	net = NeuralNet(
+    		l_u1,
+    		update=nesterov_momentum,
+    		update_learning_rate=0.01,
+    		update_momentum=0.9,
+    		max_epochs=2,
+    		verbose=1,
+    		)
+	nn=net.fit(X_train, y_train)
 
 
+def myLasagneNet(X_train, y_train):
+	l_in = InputLayer((None, 1, 819, 819))
+	l1 = Conv2DLayer(l_in, num_filters=10, filter_size=5)
+	l2 = MaxPool2DLayer(l1, pool_size=2)
+	l_u2 = InverseLayer(l2, l2)
+	l_u1 = InverseLayer(l_u2, l1)
+
+	input_var = T.tensor4('inputs')
+	target_var = T.tensor4('targets')
+
+	prediction = lasagne.layers.get_output(l_u1)
+	loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
+	loss = loss.mean()
+	params = lasagne.layers.get_all_params(l_u1, trainable=True)
+	updates = lasagne.updates.nesterov_momentum(loss, params, learning_rate=0.01, momentum=0.9)
+
+	test_prediction = lasagne.layers.get_output(l_u1, deterministic=True)
+	test_loss = lasagne.objectives.categorical_crossentropy(test_prediction,target_var)
+	test_loss = test_loss.mean()
+
+	test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var),dtype=theano.config.floatX)
 
 
 
