fb393cd28a86f848b54a0c75d5861b11edbc73b7
thanasis-com <tsiarasa@hotmail.com>
2016-06-15 15:48:32 +0100

    erased some unsed code. also added more layers to the network

--- python/test.py
+++ python/test.py
@@ -104,30 +104,32 @@ class Unpool2DLayer(lasagne.layers.Layer):
 
 
 
-#dataSet=myTools.loadImages('/home/athanasiostsiaras/Downloads/images', 1024, 1024, 4)
+dataSet=myTools.loadImages('/home/athanasiostsiaras/Downloads/images', 1024, 1024, 4)
 
-#dataSet=myTools.oneDimension(dataSet)
+dataSet=myTools.oneDimension(dataSet)
 	
 #plt.show(plt.imshow(dataSet[0][0], cmap=cm.binary))
 
-#masks=myTools.loadImages('/home/athanasiostsiaras/Downloads/masks', 819, 819, 1)
+masks=myTools.loadImages('/home/athanasiostsiaras/Downloads/masks', 819, 819, 1)
 
 #plt.show(plt.imshow(masks[0][0], cmap=cm.binary))
 
-#cropy=(1024-819)/2
-#cropx=(1024-819)/2
+cropy=(1024-819)/2
+cropx=(1024-819)/2
 
-#temp=dataSet[:,:,cropx:1024-cropx-1,cropy:1024-cropy-1]
+temp=dataSet[0:20,:,cropx:1024-cropx-1,cropy:1024-cropy-1]
 
 #myTools.myConvNet(temp,masks)
 
 
-#for x in numpy.nditer(masks, op_flags=['readwrite']):
-#     if x>0:
-#             x[...]=1
+for x in numpy.nditer(masks, op_flags=['readwrite']):
+     if x>0:
+             x[...]=1
 
-temp=numpy.random.rand(12,1,819,819)
-masks=numpy.random.random_integers(0,1, size=(12,1,815,815)).astype(numpy.int32) 
+#masks=masks.astype(numpy.int32)
+
+#temp=numpy.random.rand(12,1,819,819)
+#masks=numpy.random.random_integers(0,1, size=(12,1,819,819)).astype(numpy.int32) 
 
 data_size=(None,1,819,819)
 
@@ -140,36 +142,47 @@ net = {}
 net['data'] = lasagne.layers.InputLayer(data_size, input_var=input_var)
 
 #Convolution + Pooling
-#net['conv1'] = lasagne.layers.Conv2DLayer(net['data'], num_filters=10, filter_size=5)
-#net['pool1'] = lasagne.layers.Pool2DLayer(net['conv1'], pool_size=2)
-#net['unpool1']=lasagne.layers.InverseLayer(net['pool1'], layer=net['pool1'])
-#net['deconv1']=lasagne.layers.InverseLayer(net['unpool1'], layer=net['conv1'])
-net['conv2']=lasagne.layers.Conv2DLayer(net['data'], num_filters=1, filter_size=5)
+net['conv1'] = lasagne.layers.Conv2DLayer(net['data'], num_filters=10, filter_size=5)
+net['pool1'] = lasagne.layers.Pool2DLayer(net['conv1'], pool_size=2)
+net['conv2'] = lasagne.layers.Conv2DLayer(net['pool1'], num_filters=10, filter_size=5)
+net['pool2'] = lasagne.layers.Pool2DLayer(net['conv2'], pool_size=2)
+net['unpool2']=lasagne.layers.InverseLayer(net['pool2'], net['pool2'])
+net['deconv2']=Deconv2DLayer(net['unpool2'], num_filters=10, filter_size=5)
+net['unpool1']=lasagne.layers.InverseLayer(net['deconv2'], net['pool1'])
+net['deconv1']=Deconv2DLayer(net['unpool1'], num_filters=1, filter_size=5, nonlinearity=lasagne.nonlinearities.sigmoid)
+#net['conv2']=lasagne.layers.Conv2DLayer(net['pool1'], num_filters=1, filter_size=5)
 #net['shape']=lasagne.layers.ReshapeLayer(net['conv2'], shape= ([0], -1))
 
 print(lasagne.layers.get_output_shape(net['data']))
+print(lasagne.layers.get_output_shape(net['conv1']))
+print(lasagne.layers.get_output_shape(net['pool1']))
 print(lasagne.layers.get_output_shape(net['conv2']))
+print(lasagne.layers.get_output_shape(net['pool2']))
+print(lasagne.layers.get_output_shape(net['unpool2']))
+print(lasagne.layers.get_output_shape(net['deconv2']))
+print(lasagne.layers.get_output_shape(net['unpool1']))
+print(lasagne.layers.get_output_shape(net['deconv1']))
 #print(lasagne.layers.get_output_shape(net['shape']))
 
 lr = 0.1
 weight_decay = 1e-5
 
 #Loss function: mean cross-entropy
-prediction = lasagne.layers.get_output(net['conv2'])
-loss = lasagne.objectives.squared_error(prediction, target_var)
+prediction = lasagne.layers.get_output(net['deconv1'])
+loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
 loss = loss.mean()
 
 #Also add weight decay to the cost function
-weightsl2 = lasagne.regularization.regularize_network_params(net['conv2'], lasagne.regularization.l2)
+weightsl2 = lasagne.regularization.regularize_network_params(net['deconv1'], lasagne.regularization.l2)
 loss += weight_decay * weightsl2
 
-params = lasagne.layers.get_all_params(net['conv2'], trainable=True)
+params = lasagne.layers.get_all_params(net['deconv1'], trainable=True)
 updates = lasagne.updates.sgd(loss, params, learning_rate=lr)
 
 train_fn = theano.function([input_var, target_var], loss, updates=updates)
 
-test_prediction = lasagne.layers.get_output(net['conv2'], deterministic=True)
-test_loss = lasagne.objectives.squared_error(test_prediction,target_var)
+test_prediction = lasagne.layers.get_output(net['deconv1'], deterministic=True)
+test_loss = lasagne.objectives.categorical_crossentropy(test_prediction,target_var)
 test_loss = test_loss.mean()
 test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var),dtype=theano.config.floatX)
 
@@ -179,8 +192,8 @@ get_preds = theano.function([input_var], test_prediction)
 n_examples = temp.shape[0]
 #n_batches = n_examples / batch_size
 
-epochs=2
-n_batches=5
+epochs=10
+n_batches=20
 batch_size=1
 
 start_time = time.time()
@@ -208,6 +221,8 @@ print('Training completed in %.2f seconds.' % (end_time - start_time))
 
 
 res=get_preds(temp)
+
+#print(res[0][0])
 
 plt.show(plt.imshow(res[0][0], cmap=cm.binary))
 
