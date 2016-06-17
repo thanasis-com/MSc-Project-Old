cb8d21c324a3ed5a3101862d0be903e6279ff4b0
thanasis-com <tsiarasa@hotmail.com>
2016-06-17 11:05:03 +0100

    created myClasses.py to include deconv and unpool layers

--- python/test.py
+++ python/test.py
@@ -1,4 +1,5 @@
 import myTools
+import myClasses
 
 import theano
 import numpy
@@ -12,7 +13,6 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
 import theano.tensor as T
-import numpy
 from PIL import Image
 
 from os import listdir
@@ -27,82 +27,9 @@ from nolearn.lasagne import visualize
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 
-import lasagne
 from lasagne.layers import InputLayer, Conv2DLayer, DenseLayer, MaxPool2DLayer, InverseLayer
 
 #import transposeConv
-
-
-class Deconv2DLayer(lasagne.layers.Layer):
-
-    def __init__(self, incoming, num_filters, filter_size, stride=1, pad=0,
-            nonlinearity=lasagne.nonlinearities.rectify, **kwargs):
-        super(Deconv2DLayer, self).__init__(incoming, **kwargs)
-        self.num_filters = num_filters
-        self.filter_size = lasagne.utils.as_tuple(filter_size, 2, int)
-        self.stride = lasagne.utils.as_tuple(stride, 2, int)
-        self.pad = lasagne.utils.as_tuple(pad, 2, int)
-        self.W = self.add_param(lasagne.init.Orthogonal(),
-                (self.input_shape[1], num_filters) + self.filter_size,
-                name='W')
-        self.b = self.add_param(lasagne.init.Constant(0),
-                (num_filters,),
-                name='b')
-        if nonlinearity is None:
-            nonlinearity = lasagne.nonlinearities.identity
-        self.nonlinearity = nonlinearity
-
-    def get_output_shape_for(self, input_shape):
-        shape = tuple(i*s - 2*p + f - 1
-                for i, s, p, f in zip(input_shape[2:],
-                                      self.stride,
-                                      self.pad,
-                                      self.filter_size))
-        return (input_shape[0], self.num_filters) + shape
-
-    def get_output_for(self, input, **kwargs):
-        op = T.nnet.abstract_conv.AbstractConv2d_gradInputs(
-            imshp=self.output_shape,
-            kshp=(self.input_shape[1], self.num_filters) + self.filter_size,
-            subsample=self.stride, border_mode=self.pad)
-        conved = op(self.W, input, self.output_shape[2:])
-        if self.b is not None:
-            conved += self.b.dimshuffle('x', 0, 'x', 'x')
-        return self.nonlinearity(conved)
-
-class Unpool2DLayer(lasagne.layers.Layer):
-    """
-    This layer performs unpooling over the last two dimensions
-    of a 4D tensor.
-    """
-    def __init__(self, incoming, ds, **kwargs):
-
-        super(Unpool2DLayer, self).__init__(incoming, **kwargs)
-
-        if (isinstance(ds, int)):
-            raise ValueError('ds must have len == 2')
-        else:
-            ds = tuple(ds)
-            if len(ds) != 2:
-                raise ValueError('ds must have len == 2')
-            if ds[0] != ds[1]:
-                raise ValueError('ds should be symmetric (I am lazy)')
-            self.ds = ds
-
-    def get_output_shape_for(self, input_shape):
-        output_shape = list(input_shape)
-
-        output_shape[2] = input_shape[2] * self.ds[0]
-        output_shape[3] = input_shape[3] * self.ds[1]
-
-        return tuple(output_shape)
-
-    def get_output_for(self, input, **kwargs):
-        ds = self.ds
-        input_shape = input.shape
-        output_shape = self.get_output_shape_for(input_shape)
-        return input.repeat(2, axis=2).repeat(2, axis=3)
-
 
 
 dataSet=myTools.loadImages('/home/athanasiostsiaras/Downloads/images', 1024, 1024, 4)
@@ -151,9 +78,9 @@ net['pool1'] = lasagne.layers.Pool2DLayer(net['conv1'], pool_size=2)
 net['conv2'] = lasagne.layers.Conv2DLayer(net['pool1'], num_filters=10, filter_size=5)
 net['pool2'] = lasagne.layers.Pool2DLayer(net['conv2'], pool_size=2)
 net['unpool2']=lasagne.layers.InverseLayer(net['pool2'], net['pool2'])
-net['deconv2']=Deconv2DLayer(net['unpool2'], num_filters=10, filter_size=5)
+net['deconv2']=myClasses.Deconv2DLayer(net['unpool2'], num_filters=10, filter_size=5)
 net['unpool1']=lasagne.layers.InverseLayer(net['deconv2'], net['pool1'])
-net['deconv1']=Deconv2DLayer(net['unpool1'], num_filters=1, filter_size=5, nonlinearity=lasagne.nonlinearities.sigmoid)
+net['deconv1']=myClasses.Deconv2DLayer(net['unpool1'], num_filters=1, filter_size=5, nonlinearity=lasagne.nonlinearities.sigmoid)
 #net['conv2']=lasagne.layers.Conv2DLayer(net['pool1'], num_filters=1, filter_size=5)
 #net['shape']=lasagne.layers.ReshapeLayer(net['conv2'], shape= ([0], -1))
 
