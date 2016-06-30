276a29cd33774ce9ac278b51689aa5eff9a3ef3b
Thanasis <tsiarasa@hotmail.com>
2016-06-30 18:42:02 +0100

    Update massiveRun.sh

--- python/massiveRun.sh
+++ python/massiveRun.sh
@@ -1,5 +1,5 @@
 
-for LR in 0.0001
+for LR in 0.0001 0.0005 0.001 0.005 0.01 0.05 0.1 0.2
 do
-	THEANO_FLAGS=mode=FAST_RUN,device=$2,floatX=float32 python main.py $LR $1 1 >> res.txt
+	THEANO_FLAGS=mode=FAST_RUN,device=$2,floatX=float32 python main.py $LR $1 1000 >> res.txt
 done
