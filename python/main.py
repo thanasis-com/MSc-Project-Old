d0287ef462812ae268bb283d757ab3a95f54c6ee
thanasis-com <tsiarasa@hotmail.com>
2016-07-01 17:09:58 +0100

    bug fixes

--- python/main.py
+++ python/main.py
@@ -84,7 +84,7 @@ myNet=myTools.createNN(data_size, X=train, Y=masks[0:splitPoint, :, :, :], epoch
 
 res=myNet(test)
 
-print('Total cost on test set: %f' % (myTools.myTestCrossEntropy(res,masks[splitPoint+1:masks.shape[0], :, :, :])/len(res))
+print('Total cost on test set: %f' % (myTools.myTestCrossEntropy(res,masks[splitPoint+1:masks.shape[0], :, :, :])))
 
 
 
