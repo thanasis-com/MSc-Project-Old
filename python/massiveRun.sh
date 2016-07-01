6dbffbbe0e215c08fc4d9b1aafe7f7c98ac4b6ab
Thanasis <tsiarasa@hotmail.com>
2016-07-01 10:23:25 +0100

    Update massiveRun.sh

--- python/massiveRun.sh
+++ python/massiveRun.sh
@@ -1,5 +1,5 @@
 
 for WD in 0.0001
 do
-	THEANO_FLAGS=mode=FAST_RUN,device=$2,floatX=float32 python main.py $1 $WD 1 >> $3
+	THEANO_FLAGS=mode=FAST_RUN,device=$2,floatX=float32 python main.py $1 $WD 1000 >> $3
 done
