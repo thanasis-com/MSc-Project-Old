a4dc6f6eb87b02a68d474c9252485d2d46d378fe
Thanasis <tsiarasa@hotmail.com>
2016-07-19 13:54:34 +0100

    Update main.py

--- python/main.py
+++ python/main.py
@@ -80,7 +80,7 @@ data_size=(None,1,imgsWidth,imgsHeight)
 
 numOfBatches=14
 batchSize=math.floor(train.shape[0]/numOfBatches)
-sys.exit()
+
 
 myNet=myTools.createNN(data_size, X=train, Y=masks[0:splitPoint, :, :, :], valX=test, valY=masks[splitPoint+1:masks.shape[0], :, :, :], epochs=argEpochs, n_batches=numOfBatches, batch_size=batchSize, learning_rate=argLR, w_decay=argWD)
 
