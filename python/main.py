979374acf5cef72dd44253209d26a96b5dbc38ec
Thanasis <tsiarasa@hotmail.com>
2016-08-13 09:24:19 +0100

    Update main.py

--- python/main.py
+++ python/main.py
@@ -44,9 +44,9 @@ dataSet=myTools.oneDimension(dataSet)
 
 dataSet=dataSet.astype(numpy.uint8)
 
-dataSet=myTools.cropCenter(dataSet, 80)#81.2
+dataSet=myTools.cropCenter(dataSet, 80.6)#81.2
 
-dataSet=myTools.augmentMasks(dataSet, numOfTiles=4, overlap=False, imageWidth=819, imageHeight=819)#830
+dataSet=myTools.augmentData(dataSet, numOfTiles=4, overlap=False, imageWidth=826, imageHeight=826)#830
 	
 dataSet=dataSet.astype(numpy.float32)
 
