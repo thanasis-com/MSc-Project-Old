63cd86a053f8e012e72fa0f6ac2550e80b2b4d23
tsiarasa <tsiarasa@hotmail.com>
2016-08-22 12:18:50 +0100

    nothing important

--- python/createMasksBig.py
+++ python/createMasksBig.py
@@ -11,9 +11,9 @@ import scipy
 #get the image path
 imagePath=sys.argv[1]
 #get the threshold level
-thresh=float(sys.argv[2])
+#thresh=float(sys.argv[2])
 #get the erosion shape
-er=int(sys.argv[3])
+#er=int(sys.argv[3])
 #open the image
 image=Image.open(imagePath)
 #image as numpy array
@@ -23,9 +23,9 @@ image=image[:,:,0]
 #most needed type casting
 image=image.astype(numpy.uint8)
 #crop the center
-image=myTools.cropCenter1(image, 80)
+image=myTools.cropCenter1(image, 83.1)
 #split image to 4
-splits=myTools.augmentMasks(image.reshape(1,1,image.shape[0],image.shape[1]), numOfTiles=4, overlap=False, imageWidth=image.shape[0], imageHeight=image.shape[1])
+splits=myTools.augmentData(image.reshape(1,1,image.shape[0],image.shape[1]), numOfTiles=4, overlap=False, imageWidth=image.shape[0], imageHeight=image.shape[1])
 #another vital type casting
 splits=splits.astype(numpy.float32)
 #keep only the 4 original tiles
@@ -36,20 +36,13 @@ data_size=(None,1,splits[0][0].shape[0],splits[0][0].shape[1])
 myNet=myTools.createPretrainedNN(data_size)
 #make predictions for the 4 tiles
 res=myNet(splits)
+#crop the center of the predictions
+res=myTools.cropCenter(res, 93)
 #concatenate on the x axis
-top=np.concatenate((res[0][0],res[1][0]),axis=1)
-bot=np.concatenate((res[2][0],res[3][0]),axis=1)
+top=np.concatenate((res[0][0],res[2][0]),axis=1)
+bot=np.concatenate((res[1][0],res[3][0]),axis=1)
 #concatenate the two halves to get the full image
 res=np.concatenate((top,bot),axis=0)
 
 plt.show(plt.imshow(res, cmap=cm.binary))
-scipy.misc.imsave('imgeee.png', res)
-#threshold
-res[res>=thresh]=1
-res[res<thresh]=0
-#erode
-res=scipy.ndimage.binary_erosion(res,structure=np.ones((er,er))).astype(res.dtype)
-
-
-scipy.misc.imsave('imgee.png', res)
-plt.show(plt.imshow(res, cmap=cm.binary))
+scipy.misc.imsave('imgBig46.png', res)
